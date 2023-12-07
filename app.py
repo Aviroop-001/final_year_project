18c648a2769e301f17a478167902b46a2460b843
Aviroop-001 <banerjeeaviroop01@gmail.com>
2023-12-08 01:36:25 +0530

    returning image as response

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import cv2
 import numpy as np
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -11,25 +12,24 @@ def home_route():
 @app.route('/upscale', methods=['POST'])
 def upscale_image():
     try:
-        # Get the image from the request
         file = request.files['image']
 
         img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-
-        # Convert to grayscale if needed
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Apply histogram equalization
         enhanced_image = cv2.equalizeHist(gray)
-
-        # Convert back to color if needed
         enhanced_image = cv2.cvtColor(enhanced_image, cv2.COLOR_GRAY2BGR)
 
         # Save the enhanced image
-        enhanced_image_path = "enhanced_image.png"
-        cv2.imwrite(enhanced_image_path, enhanced_image)
+        # enhanced_image_path = "enhanced_image.png"
+        # cv2.imwrite(enhanced_image_path, enhanced_image)
+        # return jsonify({"message": "Image upscaled successfully", "upscaled_image_path": enhanced_image_path})
 
-        return jsonify({"message": "Image upscaled successfully", "upscaled_image_path": enhanced_image_path})
+        image_io = BytesIO()
+        cv2.imwrite(image_io, enhanced_image)
+        image_io.seek(0)
+
+        # Return the image in the response
+        return send_file(image_io, mimetype='image/png', as_attachment=True, download_name='enhanced_image.png')
 
     except Exception as e:
         return jsonify({"error": str(e)})
